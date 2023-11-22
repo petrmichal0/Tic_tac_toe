bbaa3a712f21c6121a6982679ea3d62b0fc47f78
Petr Michal <petrmichal0@gmail.com>
2023-11-22 01:29:16 +0100

    update styles,logic,buttons new round game, score

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import Cell from "./components/Cell";
 import { useState, useEffect } from "react";
+
+import Cell from "./components/Cell";
 
 const App = () => {
   const [cells, setCells] = useState(["", "", "", "", "", "", "", "", ""]);
   const [go, setGo] = useState("circle");
-  const [winningMessage, setWinningMessage] = useState(null);
+  const [winningMessage, setWinningMessage] = useState("");
+  const [scorePlayer1, setScorePlayer1] = useState(0);
+  const [scorePlayer2, setScorePlayer2] = useState(0);
 
-  const message = `it is now ${go} ´s go.`;
+  const message = `it is now ${go}´s go.`;
 
-  const checkScore = () => {
+  function handleNewRound() {
+    setCells(["", "", "", "", "", "", "", "", ""]);
+    setGo((go) => setGo(go === "circle" ? "circle" : "cross"));
+    setWinningMessage("");
+  }
+
+  function handleNewGame() {
+    setCells(["", "", "", "", "", "", "", "", ""]);
+    setScorePlayer1(0);
+    setScorePlayer2(0);
+    setGo("circle");
+    setWinningMessage("");
+  }
+
+  useEffect(() => {
     const winningCombos = [
       [0, 1, 2],
       [3, 4, 5],
@@ -21,30 +38,30 @@ const App = () => {
     ];
 
     winningCombos.forEach((array) => {
-      let circleWins = array.every((cell) => cells[cell] === "circle");
+      let circleWins = array.every((cell) => cells.at(cell) === "circle");
 
       if (circleWins) {
         setWinningMessage("Cicle wins");
-        return;
+        setScorePlayer1((scorePlayer1) => scorePlayer1 + 1);
       }
     });
 
     winningCombos.forEach((array) => {
-      let crossWins = array.every((cell) => cells[cell] === "cross");
+      let crossWins = array.every((cell) => cells.at(cell) === "cross");
 
       if (crossWins) {
         setWinningMessage("Cross wins");
-        return;
+        setScorePlayer2((scorePlayer2) => scorePlayer2 + 1);
       }
     });
-  };
-
-  useEffect(() => {
-    checkScore();
   }, [cells]);
 
   return (
     <div className="app">
+      <p className="text__score">
+        o {scorePlayer1}:{scorePlayer2} x
+      </p>
+
       <div className="gameboard">
         {cells.map((cell, index) => {
           return (
@@ -61,7 +78,16 @@ const App = () => {
           );
         })}
       </div>
+
       <p>{winningMessage || message}</p>
+
+      <button className="btn" onClick={handleNewRound}>
+        New Round
+      </button>
+
+      <button className="btn" onClick={handleNewGame}>
+        New Game
+      </button>
     </div>
   );
 };
